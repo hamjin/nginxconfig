ab263a12dff221df7a5414a69bced7e187b16e0e
railjty <30927889+railjty@users.noreply.github.com>
2017-08-25 08:58:35 +0800

    Update and rename ct to cert.bat

---
+++ cert.bat
@@ -0,0 +1,4 @@
+/root/ct-submit-1.1.2/ct-submit-1.1.2 ct.googleapis.com/icarus </usr/local/nginx/conf/ecc.pem >/root/ct/ecc/icarus.sct;/root/ct-submit-1.1.2/ct-submit-1.1.2 ct.googleapis.com/pilot </usr/local/nginx/conf/ecc.pem >/root/ct/ecc/pilot.sct;/root/ct-submit-1.1.2/ct-submit-1.1.2 ct.googleapis.com/icarus </usr/local/nginx/conf/rsa.pem >/root/ct/rsa/icarus.sct;/root/ct-submit-1.1.2/ct-submit-1.1.2 ct.googleapis.com/pilot </usr/local/nginx/conf/rsa.pem >/root/ct/rsa/pilot.sct;nginx -s stop;nginx
+
+
+
