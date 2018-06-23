d3ac630f7e4488066dfd95c3852348bbf62b7c4e
railjty <30927889+railjty@users.noreply.github.com>
2018-06-23 17:32:25 +0800

    Add files via upload

---
+++ fq-https.bat
@@ -0,0 +1,6 @@
+G:
+cd Desktop
+cd ss-ws
+node local.js -s 127.0.0.1 -l 1080 -m aes-256-cfb -k xyz123456 -p 80
+pause
+exit
---
+++ fq.bat
@@ -0,0 +1,6 @@
+G:
+cd Desktop
+cd ss-ws
+node local.js -s shadowsocks-wss.herokuapp.com -l 1080 -m aes-256-cfb -k xyz123456 -p 80
+pause
+exit
